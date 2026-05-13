7fa2bbb809e0e395260921f1d94f3ee6f2e63d83
Kai /3 <kayoosh2009@gmail.com>
2026-05-13 14:20:15 +0300

    Add Phone class and main method for instantiation
    Created a Phone class with model and price attributes, and a Main class to demonstrate usage.

---
+++ Java/006.java
@@ -0,0 +1,27 @@
+// - - - - - - - - - - - 
+
+public phone{
+  private String model;
+  private int price;
+
+  public phone( String _model, int _price){
+    model = _model;
+    price = _price;
+  }
+
+  public String getModel(){
+    return model;
+  }
+
+  public int getPrice(){
+    return price;
+  }
+}
+
+public class main{
+  public static void main(String [] Args ){
+      phone[] phone = new phone[3];
+
+      phone[0] = new phone ();
+  }
+}
